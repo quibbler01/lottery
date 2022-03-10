c286b64ed543305dfbb5914a5019ebf04327c605
quibbler01 <a2639168845@163.com>
2022-03-10 08:26:48 +0800

    删除不必要的文件资源,新增依赖和相关文件目录。修改主题配合使用material组件，否则报错

--- lottery/src/main/java/cn/quibbler/lottery/MainActivity.kt
+++ lottery/src/main/java/cn/quibbler/lottery/MainActivity.kt
@@ -1,13 +1,30 @@
 package cn.quibbler.lottery
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import cn.quibbler.lottery.controller.MainController
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mainController: MainController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= 28) {
+            val layoutParam: WindowManager.LayoutParams = window.attributes
+            layoutParam.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+            window.attributes = layoutParam
+        }
+
+        mainController = MainController()
+        setContentView(mainController.getView())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 
 }
