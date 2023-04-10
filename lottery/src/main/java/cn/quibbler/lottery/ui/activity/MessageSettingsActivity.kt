ad327e9fb6497548c70756adb78b9f392f9cca61
quibbler01 <fluent0418@gmail.com>
2023-04-10 12:27:57 +0800

    create settings of personal

--- lottery/src/main/java/cn/quibbler/lottery/ui/activity/MessageSettingsActivity.kt
+++ lottery/src/main/java/cn/quibbler/lottery/ui/activity/MessageSettingsActivity.kt
@@ -1,6 +1,8 @@
 package cn.quibbler.lottery.ui.activity
 
 import android.os.Bundle
+import android.widget.CompoundButton
+import android.widget.RadioGroup
 import cn.quibbler.lottery.databinding.ActivityMessageSettingsBinding
 import cn.quibbler.lottery.model.RouterCenter
 import com.alibaba.android.arouter.facade.annotation.Route
@@ -10,6 +12,19 @@ class MessageSettingsActivity : BaseActivity() {
 
     private lateinit var binding: ActivityMessageSettingsBinding
 
+    private val checkChangeListener = object : CompoundButton.OnCheckedChangeListener {
+        override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+            when (buttonView?.id) {
+                binding.allMessageRadioButton.id -> {
+                    binding.onlySubscribeMessageRadioButton.isChecked = !isChecked
+                }
+                binding.onlySubscribeMessageRadioButton.id -> {
+                    binding.allMessageRadioButton.isChecked = !isChecked
+                }
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMessageSettingsBinding.inflate(layoutInflater)
@@ -17,6 +32,13 @@ class MessageSettingsActivity : BaseActivity() {
         supportActionBar?.elevation = 0f
 
         setContentView(binding.root)
+
+        initView()
+    }
+
+    private fun initView() {
+        binding.allMessageRadioButton.setOnCheckedChangeListener(checkChangeListener)
+        binding.onlySubscribeMessageRadioButton.setOnCheckedChangeListener(checkChangeListener)
     }
 
 }
