99bea7de52a31ee810d11149ff805c4d61483e41
quibbler01 <fluent0418@gmail.com>
2023-04-16 00:10:59 +0800

    add some res and rename class name

--- lottery/src/main/java/cn/quibbler/lottery/ui/activity/MessageNewFansActivity.kt
+++ lottery/src/main/java/cn/quibbler/lottery/ui/activity/MessageNewFansActivity.kt
@@ -1,13 +1,12 @@
 package cn.quibbler.lottery.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import cn.quibbler.lottery.databinding.ActivityMessageFansBinding
 import cn.quibbler.lottery.model.RouterCenter
 import com.alibaba.android.arouter.facade.annotation.Route
 
-@Route(path = RouterCenter.message_activity_fans)
-class MessageFansActivity : BaseActivity() {
+@Route(path = RouterCenter.message_activity_new_fans)
+class MessageNewFansActivity : BaseActivity() {
 
     private lateinit var binding: ActivityMessageFansBinding
 
