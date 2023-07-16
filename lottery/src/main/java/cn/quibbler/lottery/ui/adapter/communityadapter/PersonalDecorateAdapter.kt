017e6d5f72093c8d2a57a4aa29e06f88ea07a672
quibbler01 <fluent0418@gmail.com>
2023-07-16 21:13:19 +0800

    add community personal decorate adapter

---
+++ lottery/src/main/java/cn/quibbler/lottery/ui/adapter/communityadapter/PersonalDecorateAdapter.kt
@@ -0,0 +1,23 @@
+package cn.quibbler.lottery.ui.adapter.communityadapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import cn.quibbler.lottery.databinding.CommunityRecommendPersonalDecorateBinding
+
+class PersonalDecorateAdapter : RecyclerView.Adapter<PersonalDecorateAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder(val binding: CommunityRecommendPersonalDecorateBinding) : RecyclerView.ViewHolder(binding.root)
+
+}
