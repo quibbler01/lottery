76566b09b25292b5e97a5d1ab266b5fad8148e15
quibbler01 <fluent0418@gmail.com>
2023-07-16 21:13:28 +0800

    add community voting adapter

---
+++ lottery/src/main/java/cn/quibbler/lottery/ui/adapter/communityadapter/VotingTopicsAdapter.kt
@@ -0,0 +1,23 @@
+package cn.quibbler.lottery.ui.adapter.communityadapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import cn.quibbler.lottery.databinding.CommunityRecommendVotingTopicsBinding
+
+class VotingTopicsAdapter : RecyclerView.Adapter<VotingTopicsAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder(val binding: CommunityRecommendVotingTopicsBinding) : RecyclerView.ViewHolder(binding.root)
+
+}
