4e94d443d665d8c5095ee8fab669f6b363e9fc52
quibbler01 <fluent0418@gmail.com>
2023-07-16 21:12:53 +0800

    add community post showing adapter

---
+++ lottery/src/main/java/cn/quibbler/lottery/ui/adapter/communityadapter/CommunityPostAdapter.kt
@@ -0,0 +1,23 @@
+package cn.quibbler.lottery.ui.adapter.communityadapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import cn.quibbler.lottery.databinding.CommunityRecommendPostBinding
+
+class CommunityPostAdapter : RecyclerView.Adapter<CommunityPostAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder(val binding: CommunityRecommendPostBinding) : RecyclerView.ViewHolder(binding.root)
+
+}
